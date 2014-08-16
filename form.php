fe3ce66bfd149ae4c4fa58a7288633347876930e
materechm <materechm@gmail.com>
2014-08-16 14:53:37 -0600

    Update form.php

--- form.php
+++ form.php
@@ -12,7 +12,7 @@
         if (mail ($to, $subject, $body, $from)) {
 	          echo '<p>Your message has been sent!</p>';
 	     } else {
-	    echo '<p>Something went wrong, you can send an email to me@mariachavez.co and I'll get in touch soon</p>';
+	    echo "<p>Something went wrong, you can send an email to me@mariachavez.co and I'll get in touch soon</p>";
 	}
     } else if ($_POST['submit'] && $human != '4') {
 	echo '<p>You answered the anti-spam question incorrectly!</p>';
